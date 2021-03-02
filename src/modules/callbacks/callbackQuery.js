a8bbbcbfeab2d79370f6f69bd1a1f656e09b64cf
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-02 16:19:55 +0500

    Refactor callBackQuery

--- src/modules/callbacks/callbackQuery.js
+++ src/modules/callbacks/callbackQuery.js
@@ -1,29 +1,57 @@
 const fetch = require('node-fetch')
 const CONFIG = require('./../../config/config.js')
 const KEYBOARDS = require('./keyboards/keyboards.js')
+const { makeOrder } = require('./../../lib/lib.js')
 
 module.exports = async (cb) => {
 
-	const [ key, id ] = cb.data.split(':')
+	const [ key, identifier, , product_id ] = cb.data.split(':')
 
 	try {
-		const selectedProduct = await fetch(`${CONFIG.HOST}/bot/product/${id}`)
+		const selectedProduct = await fetch(`${CONFIG.HOST}/bot/product/${identifier}`)
 
 		const { data: [ product ] } = await selectedProduct.json()
 
 		if(key === 'product') {
 
+			const caption = `Tarkibi: ${product.product_info}\nNarxi: ${product.product_price} so'm` + '\n\n<b>Miqdorni tanlang</b>'
+
 			bot.sendPhoto(
 				cb.message.chat.id,
 				product.product_image,
 				{
-					caption: product.product_info,
-					reply_markup: KEYBOARDS.replyMarkupOrderQuantity
+					caption: caption,
+					reply_markup: KEYBOARDS.replyMarkupOrderQuantity(product.product_id),
+					parse_mode: 'html'
 				}
 			)
-			
+
 		} else if (key === 'order_quantity') {
+
+			console.log(cb)
+
+			const oneClient = await fetch(`${CONFIG.HOST}/bot/client/${cb.from.id}`)
+
+			try {
+				const addNewOrder = await fetch(`${CONFIG.HOST}/bot/order`, {
+					method: 'post',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: {
+						sale_product_count: identifier, 
+						product_id: product_id, 
+						client_id: await oneClient.json(), 
+						// location_id
+					}
+				})
+			} catch(e) {
+				// statements
+				console.log(e);
+			}
 			
+		} else if (key === 'prev_menu') {
+			makeOrder(cb.message.chat.id, CONFIG)
 		}
 	} catch(e) {
 		console.log(e)
