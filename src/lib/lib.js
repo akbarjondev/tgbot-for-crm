190c08024eb39e06f5287e43aae880598c24a484
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-02 15:53:53 +0500

    Refactor allReserved products make one function

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch')
+
 function timeConverter(UNIX_timestamp){
   let a = new Date(UNIX_timestamp * 1000)
   a.toLocaleString('uz-UZ', { timeZone: 'Asia/Tashkent' })
@@ -16,6 +18,51 @@ function timeConverter(UNIX_timestamp){
   return a
 }
 
+const makeOrder = async (chatId, CONFIG) => {
+
+  try {
+    const allReservedProducts = await fetch(`${CONFIG.HOST}/bot/products`)
+
+    const { status, data } = await allReservedProducts.json()
+
+    if(status === 200) {
+
+      let inlineKeyboard = []
+      let buttonsRow = []
+
+      for (let i = 0; i < data.length; i++) {
+        buttonsRow.push({ text: data[i].product_name, callback_data: 'product:' + data[i].product_id })
+        
+        if(buttonsRow.length === 3) {
+          inlineKeyboard.push([...buttonsRow])
+          buttonsRow.length = 0
+        }
+
+        if((data.length - 1) === i) {
+          inlineKeyboard.push([...buttonsRow])
+        }
+      }
+
+      const text = `Taomlardan birni tanlang <a href="https://telegra.ph/Xayrli-tong-02-24">​</a>`
+
+      bot.sendMessage(
+        chatId,
+        text,
+        {
+          parse_mode: 'html',
+          reply_markup: JSON.stringify({
+            inline_keyboard: inlineKeyboard
+          })
+        }
+      )
+
+    }
+  } catch(e) {
+    console.log(e)
+  }
+}
+
 module.exports = {
   timeConverter,
+  makeOrder
 }
--- src/modules/callbacks/order.js
+++ src/modules/callbacks/order.js
@@ -1,49 +1,9 @@
 const fetch = require('node-fetch')
 const CONFIG = require('./../../config/config.js')
+const { makeOrder } = require('./../../lib/lib.js')
 
 module.exports = async (msg) => {
 	
-	try {
-		const allReservedProducts = await fetch(`${CONFIG.HOST}/bot/products`)
-
-    const { status, data } = await allReservedProducts.json()
-
-    if(status === 200) {
-
-      let inlineKeyboard = []
-      let buttonsRow = []
-
-      for (let i = 0; i < data.length; i++) {
-        buttonsRow.push({ text: data[i].product_name, callback_data: 'product:' + data[i].product_id })
-        
-        if(buttonsRow.length === 3) {
-          inlineKeyboard.push([...buttonsRow])
-          buttonsRow.length = 0
-        }
-
-        if((data.length - 1) === i) {
-          inlineKeyboard.push([...buttonsRow])
-        }
-      }
-
-    	const text = `Taomlardan birni tanlang <a href="https://telegra.ph/Xayrli-tong-02-24">​</a>`
-
-    	bot.sendMessage(
-    		msg.chat.id,
-    		text,
-    		{
-      		parse_mode: 'html',
-      		reply_markup: JSON.stringify({
-      			inline_keyboard: inlineKeyboard
-      		})
-    		}
-    	)
-    }
-
-
-	} catch(e) {
-		console.log(e)
-	}
-
-
+  makeOrder(msg.chat.id, CONFIG)
+	
 }
